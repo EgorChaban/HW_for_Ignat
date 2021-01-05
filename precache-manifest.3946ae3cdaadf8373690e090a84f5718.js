3636dba0a02d4c25f273135e3b19ef04f44aa055
EgorChaban <Egorkachibo@mail.ru>
2021-01-05 23:05:35 +0300

    Updates

---
+++ precache-manifest.3946ae3cdaadf8373690e090a84f5718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2e3d9ac7d3e0b8bfe2fcb5fa4a19c1",
+    "url": "/HW_for_Ignat/index.html"
+  },
+  {
+    "revision": "45514c0603d15d24387f",
+    "url": "/HW_for_Ignat/static/css/main.b85b1c32.chunk.css"
+  },
+  {
+    "revision": "c44bd3ec6080b43fe38b",
+    "url": "/HW_for_Ignat/static/js/2.d56f5b30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW_for_Ignat/static/js/2.d56f5b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45514c0603d15d24387f",
+    "url": "/HW_for_Ignat/static/js/main.df9a81e2.chunk.js"
+  },
+  {
+    "revision": "68d2a3f181ee4c297cfc",
+    "url": "/HW_for_Ignat/static/js/runtime-main.a99273e0.js"
+  }
+]);
